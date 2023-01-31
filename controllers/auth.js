129477be92d0b8d6d370efafd34fcc62ace0f230
RafoDev <rafael.david@ucsp.edu.pe>
2023-01-31 16:30:18 -0500

    Google sign in - sign out

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const { response } = require("express");
 const User = require("../models/user");
 const bcryptjs = require('bcryptjs');
 const { generateJWT } = require("../helpers/generateJWT");
+const { googleVerify } = require("../helpers/google-verify");
+
 const login = async (req, res = response) => {
 
     const { email, password } = req.body;
@@ -45,6 +47,52 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+    const { id_token } = req.body;
+    try {
+        const { email, name, img } = await googleVerify(id_token);
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            const data = {
+                name,
+                email,
+                password: ':P',
+                img,
+                google: true,
+                role: 'USER_ROLE'
+            }
+            user = new User(data);
+            await user.save();
+        }
+
+        if (!user.status) {
+            return res.status(401).json({
+                msg: 'Hable con el admin, usuario bloqueado'
+            });
+        }
+
+
+        // generar jwt
+
+        const token = await generateJWT(user.id);
+
+        return res.json({
+            msg: 'todo OK',
+            token,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        });
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
